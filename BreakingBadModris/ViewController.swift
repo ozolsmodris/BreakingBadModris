134451adb305ed0f67da68cbe167af604e9ed5a9
Modris Ozols <modris.ozols@testdevlab.com>
2020-01-14 21:44:04 +0200

    Initial Commit

---
+++ BreakingBadModris/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BreakingBadModris
+//
+//  Created by Modris Ozols on 14/01/2020.
+//  Copyright © 2020 Modris Ozols. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

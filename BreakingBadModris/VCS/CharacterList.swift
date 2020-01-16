6b34ae227de7da740e35f93674f365d3006d6520
Modris Ozols <modris.ozols@testdevlab.com>
2020-01-16 23:09:44 +0200

    app

--- BreakingBadModris/VCS/CharacterList.swift
+++ BreakingBadModris/VCS/CharacterList.swift
@@ -7,24 +7,88 @@
 //
 
 import UIKit
+import MOBreakingBadAPI
 
-class CharacterList: UIViewController {
-
+class CharacterList: UITableViewController {
+    
+    
+    var characters: [Character] = []
+    var apiController = MOBreakingBadAPI()
+    let dispatchGroup = DispatchGroup()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        fillTable()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func refresh(_ sender: UIRefreshControl) {
+        fillTable()
+        dispatchGroup.notify(queue: .main) {
+            sender.endRefreshing()
+        }
     }
-    */
+    
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.characters.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "characterCell", for: indexPath)
+        cell.textLabel?.text = self.characters[indexPath.row].name
+        return cell
+    }
+    
+    func fillTable() {
+        dispatchGroup.enter()
+        apiController.getAllCharacters { (characters) in
+            if type(of: characters) == [Character].self {
+                self.characters = characters
+                DispatchQueue.main.async {
+                    self.tableView.reloadData()
+                    self.dispatchGroup.leave()
+                }
+            }
+            else {
+                print("Character is nil!!!")
+            }
+            
+        }
 
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        
+        if segue.identifier == "toSpecificQuote",
+            let showView = segue.destination as? QuoteViewController,
+            let row = tableView.indexPathForSelectedRow?.row {
+                apiController.getQuote(from: self.characters[row].name) { (quote) in
+                    showView.hideShareButton()
+                    DispatchQueue.main.async {
+                        showView.authorLabel.text = self.characters[row].name
+                        if let quote = quote {
+                            showView.quoteLabel.text = quote.quote
+                            showView.showShareButton()
+                        } else {
+                            showView.quoteLabel.text = "No quotes found"
+                        }
+                        showView.activityIndicator.stopAnimating()
+                    }
+                }
+        } else if segue.identifier == "toRandomQuote",
+            let showView = segue.destination as? QuoteViewController {
+                apiController.getRandomQuote { (quote) in
+                    showView.hideShareButton()
+                    DispatchQueue.main.async {
+                        showView.authorLabel.text = quote?.author
+                        showView.quoteLabel.text = quote?.quote
+                        showView.showShareButton()
+                        showView.activityIndicator.stopAnimating()
+                    }
+                }
+        }
+    }
 }
---
+++ BreakingBadModris/VCS/QuoteViewController.swift
@@ -0,0 +1,51 @@
+//
+//  QuoteViewController.swift
+//  BreakingBadModris
+//
+//  Created by Modris Ozols on 16/01/2020.
+//  Copyright © 2020 Modris Ozols. All rights reserved.
+//
+
+import UIKit
+import MOBreakingBadAPI
+
+class QuoteViewController: UIViewController {
+    
+    var activityIndicator: UIActivityIndicatorView = UIActivityIndicatorView(style: .large)
+
+    @IBOutlet weak var authorLabel: UILabel!
+    @IBOutlet weak var quoteLabel: UILabel!
+    @IBOutlet weak var shareBtn: UIBarButtonItem!
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        hideShareButton()
+        activityIndicator.center = self.view.center
+        activityIndicator.hidesWhenStopped = true
+        view.addSubview(activityIndicator)
+        activityIndicator.startAnimating()
+    }
+    
+    @IBAction func shareQuote(_ sender: UIBarButtonItem) {
+        if let quote = quoteLabel.text, let author = quoteLabel.text {
+            let text = "\"\(quote)\" // \(author),"
+
+            // set up activity view controller
+            let textToShare = [ text ]
+            let activityViewController = UIActivityViewController(activityItems: textToShare, applicationActivities: nil)
+            activityViewController.popoverPresentationController?.sourceView = self.view
+            self.present(activityViewController, animated: true, completion: nil)
+        }
+    }
+    
+    func showShareButton(){
+        shareBtn.isEnabled = true
+        shareBtn.tintColor = nil
+    }
+
+    func hideShareButton(){
+        shareBtn.isEnabled = false
+        shareBtn.tintColor = .clear
+    }
+    
+}
